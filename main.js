40cf88ca4063d9b74c90d3b8a8828231ca118be7
Sardor <sardorkobilzonov4@gmail.com>
2025-04-16 23:23:19 +0500

    first commit

--- main.js
+++ main.js
@@ -1,177 +1,83 @@
 "use strict";
-// IF DAGI 10 TA MASALA
+// IF DAGI 5 TA MASALA
 //1-MASALA
-{
-    function butunMusbatSon(num) {
-        if (num > 0) {
-            return num + 1;
-        }
-        else {
-            return num;
-        }
-    }
-    const inputNumber = 3;
-    const result = butunMusbatSon(inputNumber);
-    console.log(result);
-}
+// {
+//   function butunMusbatSon(num: number): number {
+//     if (num > 0) {
+//       return num + 1;
+//     } else {
+//       return num;
+//     }
+//   }
+//   const inputNumber: number = 3;
+//   const result = butunMusbatSon(inputNumber);
+//   console.log(result);
+// }
 //2-MASALA
-{
-    function MusbatSonniYangilash(son) {
-        if (son > 0) {
-            return son + 1;
-        }
-        else {
-            return son - 2;
-        }
-    }
-    const inputNumber = -3;
-    const result = MusbatSonniYangilash(inputNumber);
-    console.log(result);
-}
+// {
+//   function MusbatSonniYangilash(son: number): number {
+//     if (son > 0) {
+//       return son + 1;
+//     } else {
+//       return son - 2;
+//     }
+//   }
+//   const inputNumber: number = -3;
+//   const result = MusbatSonniYangilash(inputNumber);
+//   console.log(result);
+// }
 //3-MASALA
-{
-    function sonniQaytaIshla(son) {
-        if (son > 0) {
-            return son + 1;
-        }
-        else if (son < 0) {
-            return son - 2;
-        }
-        else {
-            return 10;
-        }
-    }
-    const inputNumber = 0;
-    const result = sonniQaytaIshla(inputNumber);
-    console.log(result);
-}
+// {
+//   function sonniQaytaIshla(son: number): number {
+//     if (son > 0) {
+//       return son + 1;
+//     } else if (son < 0) {
+//       return son - 2;
+//     } else {
+//       return 10;
+//     }
+//   }
+//   const inputNumber: number = 0;
+//   const result = sonniQaytaIshla(inputNumber);
+//   console.log(result);
+// }
 //4-MASALA
-{
-    function musbatSonlarSoni(son1, son2, son3) {
-        let count = 0;
-        if (son1 > 0)
-            count++;
-        if (son2 > 0)
-            count++;
-        if (son3 > 0)
-            count++;
-        return count;
-    }
-    const result = musbatSonlarSoni(5, -2, 0);
-    console.log("Musbat sonlar soni:", result);
-}
+// {
+//   function musbatSonlarSoni(son1: number, son2: number, son3: number): number {
+//     let count: number = 0;
+//     if (son1 > 0) count++;
+//     if (son2 > 0) count++;
+//     if (son3 > 0) count++;
+//     return count;
+//   }
+//   const result = musbatSonlarSoni(5, -2, 0);
+//   console.log("Musbat sonlar soni:", result);
+// }
 //5-MASALA
-{
-    function musbatVaManfiySonlarSoni(a, b, c) {
-        let musbatCount = 0;
-        let manfiyCount = 0;
-        if (a > 0)
-            musbatCount++;
-        if (a < 0)
-            manfiyCount++;
-        if (b > 0)
-            musbatCount++;
-        if (b < 0)
-            manfiyCount++;
-        if (c > 0)
-            musbatCount++;
-        if (c < 0)
-            manfiyCount++;
-        return { musbat: musbatCount, manfiy: manfiyCount };
-    }
-    const num1 = 4;
-    const num2 = -1;
-    const num3 = -6;
-    const result = musbatVaManfiySonlarSoni(num1, num2, num3);
-    console.log(`Musbat sonlar soni: ${result.musbat}`);
-    console.log(`Manfiy sonlar soni: ${result.manfiy}`);
-}
-//6-MASALA
-{
-    function kattasiniAniqlash(son1, son2) {
-        if (son1 > son2) {
-            return son1;
-        }
-        else {
-            return son2;
-        }
-    }
-    const num1 = 5;
-    const num2 = 8;
-    const result = kattasiniAniqlash(num1, num2);
-    console.log("Kattasi", result);
-}
-//7-MASALA
-{
-    function kichiginiAniqlash(son1, son2) {
-        if (son1 < son2) {
-            return `Birinchi son kichik: ${son1}`;
-        }
-        else if (son2 < son1) {
-            return `Ikkinchi son kichik: ${son2}`;
-        }
-        else {
-            return "Ikkala son teng.";
-        }
-    }
-    const num1 = 7;
-    const num2 = 3;
-    const result = kichiginiAniqlash(num1, num2);
-    console.log(result);
-}
-//8-MASALA
-{
-    function kattasiVaKichiginiAniqlash(son1, son2) {
-        if (son1 > son2) {
-            console.log("Kattaroq son:", son1);
-            console.log("Kichikroq son:", son2);
-        }
-        else if (son2 > son1) {
-            console.log("Kattaroq son:", son2);
-            console.log("Kichikroq son:", son1);
-        }
-        else {
-            console.log("Ikkala son teng:", son1);
-        }
-    }
-    const num1 = 9;
-    const num2 = 4;
-    kattasiVaKichiginiAniqlash(num1, num2);
-}
-//9-MASALA
-{
-    function ozgartirishAgarKerak(a, b) {
-        if (a > b) {
-            let temp = a;
-            a = b;
-            b = temp;
-        }
-        console.log("A soni:", a);
-        console.log("B soni:", b);
-    }
-    let a = 10.5;
-    let b = 5.5;
-    ozgartirishAgarKerak(a, b);
-}
-//10-MASALA
-{
-    function ozgartirishAgarKerak(a, b) {
-        if (a !== b) {
-            a = a + b;
-            b = a;
-        }
-        else {
-            a = 0;
-            b = 0;
-        }
-        console.log("A soni:", a);
-        console.log("B soni:", b);
-    }
-    let a = 5;
-    let b = 8;
-    ozgartirishAgarKerak(a, b);
-}
-//  BEGIN DAGI 10 TA MASALA
+// {
+//   function musbatVaManfiySonlarSoni(
+//     a: number,
+//     b: number,
+//     c: number
+//   ): { musbat: number; manfiy: number } {
+//     let musbatCount = 0;
+//     let manfiyCount = 0;
+//     if (a > 0) musbatCount++;
+//     if (a < 0) manfiyCount++;
+//     if (b > 0) musbatCount++;
+//     if (b < 0) manfiyCount++;
+//     if (c > 0) musbatCount++;
+//     if (c < 0) manfiyCount++;
+//     return { musbat: musbatCount, manfiy: manfiyCount };
+//   }
+//   const num1 = 4;
+//   const num2 = -1;
+//   const num3 = -6;
+//   const result = musbatVaManfiySonlarSoni(num1, num2, num3);
+//   console.log(`Musbat sonlar soni: ${result.musbat}`);
+//   console.log(`Manfiy sonlar soni: ${result.manfiy}`);
+// }
+//  BEGIN DAGI 12 TA MASALA
 console.log("BEWGIN MASALA..........");
 //1-MASALA
 {
@@ -208,7 +114,13 @@ console.log("BEWGIN MASALA..........");
 }
 //4-MASALA
 {
-    // buni chunmadim shuning uchun qila olmadim
+    function circleLength(diameter) {
+        const pi = 3.14;
+        const length = pi * diameter;
+        return length;
+    }
+    const d = 10;
+    console.log(`Aylananing uzunligi: ${circleLength(d)}`); // 31.4
 }
 //5-MASALA
 {
@@ -226,21 +138,115 @@ console.log("BEWGIN MASALA..........");
 }
 //6-MASALA
 {
-    function parallelepipedHajmi(a, b, c) {
-        return a * b * c;
+    function parallelepipedHajmiVaSirti(a, b, c) {
+        const hajmi = a * b * c;
+        const sirti = 2 * (a * b + b * c + a * c);
+        return {
+            hajmi,
+            sirti,
+        };
     }
-    function parallelepipedTolaSirti(a, b, c) {
-        return 2 * (a * b + b * c + a * c);
+    const a = 3, b = 4, c = 5;
+    const natija = parallelepipedHajmiVaSirti(a, b, c);
+    console.log(`Parallelepipedning hajmi (V): ${natija.hajmi}`);
+    console.log(`Parallelepipedning to'la sirti (S): ${natija.sirti}`);
+}
+//7-MASALA
+{
+    function doiraUzunligiVaYuzasi(R) {
+        const pi = 3.14;
+        const uzunligi = 2 * pi * R;
+        const yuzasi = pi * R * R;
+        return {
+            uzunligi,
+            yuzasi,
+        };
+    }
+    const R = 5;
+    const natija = doiraUzunligiVaYuzasi(R);
+    console.log(`Doiraning uzunligi (L): ${natija.uzunligi}`);
+    console.log(`Doiraning yuzasi (S): ${natija.yuzasi}`);
+}
+//8-MASALA
+{
+    function ikkitaSonOrtaArifmetigi(a, b) {
+        return (a + b) / 2;
+    }
+    const a = 8;
+    const b = 12;
+    const natija = ikkitaSonOrtaArifmetigi(a, b);
+    console.log(`Ikkita sonning o‘rta arifmetigi: ${natija}`);
+}
+//9-MASALA
+{
+    function ikkitaSonOrtaGeometrigi(a, b) {
+        if (a < 0 || b < 0) {
+            throw new Error("Ikkita son ham manfiy bo‘lmagan bo‘lishi kerak.");
+        }
+        return Math.sqrt(a * b);
+    }
+    const a = 9;
+    const b = 16;
+    const natija = ikkitaSonOrtaGeometrigi(a, b);
+    console.log(`Ikkita sonning o‘rta geometrigi: ${natija}`);
+}
+//10-MASALA
+{
+    function nolgaTengBolmaganSonlarNatijalari(a, b) {
+        if (a === 0 || b === 0) {
+            throw new Error("Ikkala son ham nolga teng bo‘lmasligi kerak.");
+        }
+        const yigindi = a + b;
+        const kopaytma = a * b;
+        const kvadratiA = a * a;
+        const kvadratiB = b * b;
+        return { yigindi, kopaytma, kvadratiA, kvadratiB };
+    }
+    const a = 7;
+    const b = 3;
+    const natija = nolgaTengBolmaganSonlarNatijalari(a, b);
+    console.log(`Yig‘indi: ${natija.yigindi}`);
+    console.log(`Ko‘paytma: ${natija.kopaytma}`);
+    console.log(`Kvadrati (a²): ${natija.kvadratiA}`);
+    console.log(`Kvadrati (b²): ${natija.kvadratiB}`);
+}
+//11-MASALA
+{
+    function nolgaTengBolmaganSonlarNatijalari(a, b) {
+        if (a === 0 || b === 0) {
+            throw new Error("Ikkala son ham nolga teng bo‘lmasligi kerak.");
+        }
+        const yigindi = a + b;
+        const kopaytma = a * b;
+        const modulA = Math.abs(a);
+        const modulB = Math.abs(b);
+        return { yigindi, kopaytma, modulA, modulB };
+    }
+    const a = -8;
+    const b = 5;
+    const natija = nolgaTengBolmaganSonlarNatijalari(a, b);
+    console.log(`Yig‘indi: ${natija.yigindi}`);
+    console.log(`Ko‘paytma: ${natija.kopaytma}`);
+    console.log(`Modul(a): ${natija.modulA}`);
+    console.log(`Modul(b): ${natija.modulB}`);
+}
+//12-MASALA
+{
+    function togriUchburchakGipotenuzaVaPerimetr(a, b) {
+        if (a <= 0 || b <= 0) {
+            throw new Error("Katetlar musbat bo‘lishi kerak.");
+        }
+        const gipotenuza = Math.sqrt(a * a + b * b);
+        const perimetr = a + b + gipotenuza;
+        return { gipotenuza, perimetr };
     }
     const a = 3;
     const b = 4;
-    const c = 5;
-    const V = parallelepipedHajmi(a, b, c);
-    const S = parallelepipedTolaSirti(a, b, c);
-    console.log("Parallelepipedning hajmi (V):", V);
-    console.log("Parallelepipedning to‘la sirti (S):", S);
+    const natija = togriUchburchakGipotenuzaVaPerimetr(a, b);
+    console.log(`Gipotenuza (c): ${natija.gipotenuza}`);
+    console.log(`Perimetr (P): ${natija.perimetr}`);
 }
-//  BOOLEAN DAGI 10 TA MASALA
+//  BOOLEAN DAGI 5 TA MASALA
 console.log("BOOLEAN MASALA............");
 //1-MASALA
 {
--- main.ts
+++ main.ts
@@ -1,199 +1,103 @@
-// IF DAGI 10 TA MASALA
+// IF DAGI 5 TA MASALA
 
 //1-MASALA
-{
-  function butunMusbatSon(num: number): number {
-    if (num > 0) {
-      return num + 1;
-    } else {
-      return num;
-    }
-  }
+// {
+//   function butunMusbatSon(num: number): number {
+//     if (num > 0) {
+//       return num + 1;
+//     } else {
+//       return num;
+//     }
+//   }
 
-  const inputNumber: number = 3;
-  const result = butunMusbatSon(inputNumber);
+//   const inputNumber: number = 3;
+//   const result = butunMusbatSon(inputNumber);
 
-  console.log(result);
-}
+//   console.log(result);
+// }
 
 //2-MASALA
-{
-  function MusbatSonniYangilash(son: number): number {
-    if (son > 0) {
-      return son + 1;
-    } else {
-      return son - 2;
-    }
-  }
+// {
+//   function MusbatSonniYangilash(son: number): number {
+//     if (son > 0) {
+//       return son + 1;
+//     } else {
+//       return son - 2;
+//     }
+//   }
 
-  const inputNumber: number = -3;
-  const result = MusbatSonniYangilash(inputNumber);
+//   const inputNumber: number = -3;
+//   const result = MusbatSonniYangilash(inputNumber);
 
-  console.log(result);
-}
+//   console.log(result);
+// }
 
 //3-MASALA
-{
-  function sonniQaytaIshla(son: number): number {
-    if (son > 0) {
-      return son + 1;
-    } else if (son < 0) {
-      return son - 2;
-    } else {
-      return 10;
-    }
-  }
+// {
+//   function sonniQaytaIshla(son: number): number {
+//     if (son > 0) {
+//       return son + 1;
+//     } else if (son < 0) {
+//       return son - 2;
+//     } else {
+//       return 10;
+//     }
+//   }
 
-  const inputNumber: number = 0;
-  const result = sonniQaytaIshla(inputNumber);
+//   const inputNumber: number = 0;
+//   const result = sonniQaytaIshla(inputNumber);
 
-  console.log(result);
-}
+//   console.log(result);
+// }
 
 //4-MASALA
-{
-  function musbatSonlarSoni(son1: number, son2: number, son3: number): number {
-    let count: number = 0;
+// {
+//   function musbatSonlarSoni(son1: number, son2: number, son3: number): number {
+//     let count: number = 0;
 
-    if (son1 > 0) count++;
-    if (son2 > 0) count++;
-    if (son3 > 0) count++;
+//     if (son1 > 0) count++;
+//     if (son2 > 0) count++;
+//     if (son3 > 0) count++;
 
-    return count;
-  }
+//     return count;
+//   }
 
-  const result = musbatSonlarSoni(5, -2, 0);
-  console.log("Musbat sonlar soni:", result);
-}
+//   const result = musbatSonlarSoni(5, -2, 0);
+//   console.log("Musbat sonlar soni:", result);
+// }
 
 //5-MASALA
-{
-  function musbatVaManfiySonlarSoni(
-    a: number,
-    b: number,
-    c: number
-  ): { musbat: number; manfiy: number } {
-    let musbatCount = 0;
-    let manfiyCount = 0;
+// {
+//   function musbatVaManfiySonlarSoni(
+//     a: number,
+//     b: number,
+//     c: number
+//   ): { musbat: number; manfiy: number } {
+//     let musbatCount = 0;
+//     let manfiyCount = 0;
 
-    if (a > 0) musbatCount++;
-    if (a < 0) manfiyCount++;
+//     if (a > 0) musbatCount++;
+//     if (a < 0) manfiyCount++;
 
-    if (b > 0) musbatCount++;
-    if (b < 0) manfiyCount++;
+//     if (b > 0) musbatCount++;
+//     if (b < 0) manfiyCount++;
 
-    if (c > 0) musbatCount++;
-    if (c < 0) manfiyCount++;
+//     if (c > 0) musbatCount++;
+//     if (c < 0) manfiyCount++;
 
-    return { musbat: musbatCount, manfiy: manfiyCount };
-  }
+//     return { musbat: musbatCount, manfiy: manfiyCount };
+//   }
 
-  const num1 = 4;
-  const num2 = -1;
-  const num3 = -6;
+//   const num1 = 4;
+//   const num2 = -1;
+//   const num3 = -6;
 
-  const result = musbatVaManfiySonlarSoni(num1, num2, num3);
-  console.log(`Musbat sonlar soni: ${result.musbat}`);
-  console.log(`Manfiy sonlar soni: ${result.manfiy}`);
-}
+//   const result = musbatVaManfiySonlarSoni(num1, num2, num3);
+//   console.log(`Musbat sonlar soni: ${result.musbat}`);
+//   console.log(`Manfiy sonlar soni: ${result.manfiy}`);
+// }
 
-//6-MASALA
-{
-  function kattasiniAniqlash(son1: number, son2: number): number {
-    if (son1 > son2) {
-      return son1;
-    } else {
-      return son2;
-    }
-  }
-
-  const num1 = 5;
-  const num2 = 8;
-
-  const result = kattasiniAniqlash(num1, num2);
-  console.log("Kattasi", result);
-}
-
-//7-MASALA
-{
-  function kichiginiAniqlash(son1: number, son2: number): string {
-    if (son1 < son2) {
-      return `Birinchi son kichik: ${son1}`;
-    } else if (son2 < son1) {
-      return `Ikkinchi son kichik: ${son2}`;
-    } else {
-      return "Ikkala son teng.";
-    }
-  }
-
-  const num1 = 7;
-  const num2 = 3;
-
-  const result = kichiginiAniqlash(num1, num2);
-  console.log(result);
-}
-
-//8-MASALA
-{
-  function kattasiVaKichiginiAniqlash(son1: number, son2: number): void {
-    if (son1 > son2) {
-      console.log("Kattaroq son:", son1);
-      console.log("Kichikroq son:", son2);
-    } else if (son2 > son1) {
-      console.log("Kattaroq son:", son2);
-      console.log("Kichikroq son:", son1);
-    } else {
-      console.log("Ikkala son teng:", son1);
-    }
-  }
-
-  const num1 = 9;
-  const num2 = 4;
-
-  kattasiVaKichiginiAniqlash(num1, num2);
-}
-
-//9-MASALA
-{
-  function ozgartirishAgarKerak(a: number, b: number): void {
-    if (a > b) {
-      let temp = a;
-      a = b;
-      b = temp;
-    }
-
-    console.log("A soni:", a);
-    console.log("B soni:", b);
-  }
-
-  let a = 10.5;
-  let b = 5.5;
-
-  ozgartirishAgarKerak(a, b);
-}
-
-//10-MASALA
-{
-  function ozgartirishAgarKerak(a: number, b: number): void {
-    if (a !== b) {
-      a = a + b;
-      b = a;
-    } else {
-      a = 0;
-      b = 0;
-    }
-
-    console.log("A soni:", a);
-    console.log("B soni:", b);
-  }
-
-  let a = 5;
-  let b = 8;
-
-  ozgartirishAgarKerak(a, b);
-}
-
-//  BEGIN DAGI 10 TA MASALA
+//  BEGIN DAGI 12 TA MASALA
 console.log("BEWGIN MASALA..........");
 
 //1-MASALA
@@ -242,7 +146,14 @@ console.log("BEWGIN MASALA..........");
 
 //4-MASALA
 {
-  // buni chunmadim shuning uchun qila olmadim
+  function circleLength(diameter: number): number {
+    const pi = 3.14;
+    const length = pi * diameter;
+    return length;
+  }
+
+  const d = 10;
+  console.log(`Aylananing uzunligi: ${circleLength(d)}`); // 31.4
 }
 
 //5-MASALA
@@ -266,26 +177,163 @@ console.log("BEWGIN MASALA..........");
 
 //6-MASALA
 {
-  function parallelepipedHajmi(a: number, b: number, c: number): number {
-    return a * b * c;
+  function parallelepipedHajmiVaSirti(
+    a: number,
+    b: number,
+    c: number
+  ): { hajmi: number; sirti: number } {
+    const hajmi = a * b * c;
+    const sirti = 2 * (a * b + b * c + a * c);
+
+    return {
+      hajmi,
+      sirti,
+    };
   }
 
-  function parallelepipedTolaSirti(a: number, b: number, c: number): number {
-    return 2 * (a * b + b * c + a * c);
+  const a = 3,
+    b = 4,
+    c = 5;
+  const natija = parallelepipedHajmiVaSirti(a, b, c);
+  console.log(`Parallelepipedning hajmi (V): ${natija.hajmi}`);
+  console.log(`Parallelepipedning to'la sirti (S): ${natija.sirti}`);
+}
+
+//7-MASALA
+{
+  function doiraUzunligiVaYuzasi(R: number): {
+    uzunligi: number;
+    yuzasi: number;
+  } {
+    const pi = 3.14;
+    const uzunligi = 2 * pi * R;
+    const yuzasi = pi * R * R;
+
+    return {
+      uzunligi,
+      yuzasi,
+    };
+  }
+
+  const R = 5;
+  const natija = doiraUzunligiVaYuzasi(R);
+  console.log(`Doiraning uzunligi (L): ${natija.uzunligi}`);
+  console.log(`Doiraning yuzasi (S): ${natija.yuzasi}`);
+}
+
+//8-MASALA
+{
+  function ikkitaSonOrtaArifmetigi(a: number, b: number): number {
+    return (a + b) / 2;
+  }
+
+  const a = 8;
+  const b = 12;
+  const natija = ikkitaSonOrtaArifmetigi(a, b);
+  console.log(`Ikkita sonning o‘rta arifmetigi: ${natija}`);
+}
+
+//9-MASALA
+{
+  function ikkitaSonOrtaGeometrigi(a: number, b: number): number {
+    if (a < 0 || b < 0) {
+      throw new Error("Ikkita son ham manfiy bo‘lmagan bo‘lishi kerak.");
+    }
+    return Math.sqrt(a * b);
+  }
+
+  const a = 9;
+  const b = 16;
+  const natija = ikkitaSonOrtaGeometrigi(a, b);
+  console.log(`Ikkita sonning o‘rta geometrigi: ${natija}`);
+}
+
+//10-MASALA
+{
+  function nolgaTengBolmaganSonlarNatijalari(
+    a: number,
+    b: number
+  ): {
+    yigindi: number;
+    kopaytma: number;
+    kvadratiA: number;
+    kvadratiB: number;
+  } {
+    if (a === 0 || b === 0) {
+      throw new Error("Ikkala son ham nolga teng bo‘lmasligi kerak.");
+    }
+
+    const yigindi = a + b;
+    const kopaytma = a * b;
+    const kvadratiA = a * a;
+    const kvadratiB = b * b;
+
+    return { yigindi, kopaytma, kvadratiA, kvadratiB };
+  }
+
+  const a = 7;
+  const b = 3;
+  const natija = nolgaTengBolmaganSonlarNatijalari(a, b);
+  console.log(`Yig‘indi: ${natija.yigindi}`);
+  console.log(`Ko‘paytma: ${natija.kopaytma}`);
+  console.log(`Kvadrati (a²): ${natija.kvadratiA}`);
+  console.log(`Kvadrati (b²): ${natija.kvadratiB}`);
+}
+
+//11-MASALA
+{
+  function nolgaTengBolmaganSonlarNatijalari(
+    a: number,
+    b: number
+  ): {
+    yigindi: number;
+    kopaytma: number;
+    modulA: number;
+    modulB: number;
+  } {
+    if (a === 0 || b === 0) {
+      throw new Error("Ikkala son ham nolga teng bo‘lmasligi kerak.");
+    }
+
+    const yigindi = a + b;
+    const kopaytma = a * b;
+    const modulA = Math.abs(a);
+    const modulB = Math.abs(b);
+
+    return { yigindi, kopaytma, modulA, modulB };
+  }
+
+  const a = -8;
+  const b = 5;
+  const natija = nolgaTengBolmaganSonlarNatijalari(a, b);
+  console.log(`Yig‘indi: ${natija.yigindi}`);
+  console.log(`Ko‘paytma: ${natija.kopaytma}`);
+  console.log(`Modul(a): ${natija.modulA}`);
+  console.log(`Modul(b): ${natija.modulB}`);
+}
+
+//12-MASALA
+{
+  function togriUchburchakGipotenuzaVaPerimetr(
+    a: number,
+    b: number
+  ): { gipotenuza: number; perimetr: number } {
+    if (a <= 0 || b <= 0) {
+      throw new Error("Katetlar musbat bo‘lishi kerak.");
+    }
+    const gipotenuza = Math.sqrt(a * a + b * b);
+    const perimetr = a + b + gipotenuza;
+    return { gipotenuza, perimetr };
   }
 
   const a = 3;
   const b = 4;
-  const c = 5;
-
-  const V = parallelepipedHajmi(a, b, c);
-  const S = parallelepipedTolaSirti(a, b, c);
-
-  console.log("Parallelepipedning hajmi (V):", V);
-  console.log("Parallelepipedning to‘la sirti (S):", S);
+  const natija = togriUchburchakGipotenuzaVaPerimetr(a, b);
+  console.log(`Gipotenuza (c): ${natija.gipotenuza}`);
+  console.log(`Perimetr (P): ${natija.perimetr}`);
 }
 
-//  BOOLEAN DAGI 10 TA MASALA
+//  BOOLEAN DAGI 5 TA MASALA
 console.log("BOOLEAN MASALA............");
 
 //1-MASALA
